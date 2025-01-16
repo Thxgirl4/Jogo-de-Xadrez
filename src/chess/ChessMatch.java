1ea35a0f47dc0683b4c2d89cec4f45b3f9d07a46
AnaJu <anajuliasilva3304@gmail.com>
2025-01-16 01:22:02 -0300

    implementing possible moves of Rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,6 +28,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece)capturedPiece;
     }
@@ -41,8 +42,15 @@ public class ChessMatch {
         if(!board.thereIsAPiece(position)){
             throw new ChessException("there is no piece on source");
         }
+        if(!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("there is no possible moves in this place");
+        }
     }
-
+    private void validateTargetPosition(Position source, Position target){
+        if(!board.piece(source).possibleMove(target)){
+            throw new ChessException("the chosen piece can't move to target position");
+        }
+    }
 
     private void InitialSetup(){
       placeNewPiece('b', (char) 6, new Rook(board, Color.WHITE));
--- src/chess/Pieces/Rook.java
+++ src/chess/Pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.Pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -16,6 +17,48 @@ public class Rook extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-        return mat;
+
+        Position p = new Position(0,0);
+
+        //logica acima da peça torre
+        p.setValues(position.getRow() -1, position.getColumn());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat [p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() - 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //logica movimento para a esquerda
+        p.setValues(position.getRow(), position.getColumn() - 1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat [p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() - 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //logica movimento para a direita
+        p.setValues(position.getRow(), position.getColumn() + 1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat [p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() + 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //logica movimento para baixo
+        p.setValues(position.getRow() + 1, position.getColumn());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat [p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() + 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+      return mat;
     }
 }
